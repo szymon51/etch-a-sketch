072f71c7561c87e81523d5d9823e2d5dc8c97214
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-04-14 12:48:38 +0100

    Fix color chaning

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const grid = document.querySelector('#grid');
-
-for (let i = 1; i <= 16; i++) {
-    const gridElement = document.createElement('div');
-    grid.appendChild(gridElement);
+function createGrid() {
+    for (let i = 1; i <= 16; i++) {
+        const div = document.createElement('div');
+        grid.appendChild(div);
+    }
 }
+
+function changeColor() {
+    const gridElement = document.querySelectorAll("#grid > div");
+    gridElement.forEach(element => {
+        element.addEventListener("mouseover", (event) => {
+            element.style.backgroundColor = "black";
+        }), 
+        element.addEventListener("mouseout", (event) => {
+            element.style.backgroundColor = "white";
+        })
+    });
+}
+
+const grid = document.querySelector('#grid');
+createGrid();
+changeColor();
