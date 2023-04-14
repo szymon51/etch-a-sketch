77bb2e3abcc239548a580767dd130bf25eadcb6e
Szymon51 <120176777+szymon51@users.noreply.github.com>
2023-04-14 15:31:18 +0100

    Fix bugs

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-function createGrid() {
-    for (let i = 1; i <= 16; i++) {
+function newGrid() {
+    askForNumber();
+    removeGrid();
+    createGrid(numberOfSquares);
+    gridElement = document.querySelectorAll("#grid > div");
+    addChangeColor();
+}
+
+function createGrid(squaresPerSide) {
+    for (let i = 1; i <= squaresPerSide**2; i++) {
         const div = document.createElement('div');
         grid.appendChild(div);
     }
 }
 
-function changeColor() {
-    const gridElement = document.querySelectorAll("#grid > div");
+function removeGrid() {
+   gridElement.forEach(element => element.remove()); 
+}
+
+function addChangeColor() {
     gridElement.forEach(element => {
         element.addEventListener("mouseover", (event) => {
             element.classList.add("hover");
@@ -17,6 +28,16 @@ function changeColor() {
     });
 }
 
+function askForNumber() {
+    numberOfSquares = prompt("Enter the amounth of squares per side");
+    if (numberOfSquares > 100) alert("The number you have entered is too large!");
+}
+
+let numberOfSquares = 4;
 const grid = document.querySelector('#grid');
-createGrid();
-changeColor();
+createGrid(numberOfSquares);
+let gridElement = document.querySelectorAll("#grid > div");
+addChangeColor();
+
+const button = document.querySelector('button');
+button.addEventListener('click', newGrid);
